44e0f037d3b624c344b7412b85b6d7e9a2a33b50
Matveev Stepan Andreevich <mset321@gmail.com>
2025-08-03 19:31:27 +0300

    feat: add random in second part

---
+++ 2.4. random/hello/hello.go
@@ -0,0 +1,24 @@
+package main
+
+// go mod edit -replace example.com/greetings=../greetings
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	msg, err := greetings.Hello("Stepan")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(msg)
+}
